cd278204c15f0afe481a62f4497544d96f04df5b
Justin Woo <moomoowoo@gmail.com>
2016-04-14 14:04:55 +0300

    init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+requires config.json with stuff
---
+++ index.js
@@ -0,0 +1,69 @@
+var spawn = require('child_process').spawn;
+var path = require('path');
+var Cycle = require('@cycle/core');
+var Rx = require('rx');
+var TelegramBot = require('node-telegram-bot-api');
+
+var config = require('./config');
+var token = config.token;
+var torscraperPath = config.torscraperPath;
+
+function botDriver(input$) {
+  var requests$ = new Rx.ReplaySubject(1);
+  var bot = new TelegramBot(token, {polling: true});
+
+  function sendMessage(fromId, resp) {
+    console.log('responding to user ', fromId);
+    console.log(resp);
+    bot.sendMessage(fromId, resp);
+  }
+
+  bot.onText(/^gs$/, function (msg, match) {
+    var fromId = msg.from.id;
+    console.log('processing getshows');
+    requests$.onNext(fromId);
+  });
+
+  input$.subscribe(function (input) {
+    sendMessage(input.id, input.output);
+  });
+
+  console.log('boot sequence finished!!!');
+
+  return requests$;
+}
+
+function torscraperDriver(requests$) {
+  var output$ = new Rx.ReplaySubject(1);
+
+  requests$.subscribe(function (id) {
+    console.log('processing request from ', id);
+    var output = '';
+    var torscraper = spawn('node', ['index.js'], {cwd: torscraperPath});
+    torscraper.stdout.on('data', function (data) {
+      console.log('have data', data);
+      output += data;
+    });
+    torscraper.on('close', function () {
+      console.log('close', output);
+      output$.onNext({
+        id: id,
+        output: output
+      });
+    });
+  });
+
+  return output$;
+}
+
+function main(sources) {
+  return {
+    bot: sources.torscraper,
+    torscraper: sources.bot
+  };
+}
+
+Cycle.run(main, {
+  bot: botDriver,
+  torscraper: torscraperDriver
+});
