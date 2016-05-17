7ebfa4aa688ff824f1e305688aad6b6983b6790a
Justin Woo <moomoowoo@gmail.com>
2016-05-17 15:10:45 +0300

    Convert code to purescript with some hacky and messy callbacks
    Should figure out a better way to do these later that doesn't involve
    using the same old reactive solutions

---
+++ src/Main.js
@@ -0,0 +1,99 @@
+// module Main
+
+var spawn = require('child_process').spawn;
+var path = require('path');
+var TelegramBot = require('node-telegram-bot-api');
+
+exports.parseConfig = function (string) {
+  var config = JSON.parse(string);
+
+  if (!config.token) throw "missing {token} in config (telegram bot token)"
+  if (!config.torscraperPath) throw "missing {torscraperPath} in config (path to torscraper)"
+  if (!config.master) throw "missing {master} in config (numeric id of telegram master)"
+
+  return config;
+}
+
+exports.runTorscraper = function (torscraperPath) {
+  return function (callback) {
+    return function (request) {
+      return function () {
+        var origin = request.origin;
+        var id = request.id;
+        var output = '';
+        var torscraper = spawn('node', ['index.js'], {cwd: torscraperPath});
+        console.log('processing request from', origin);
+        torscraper.stdout.on('data', function (data) {
+          output += data;
+        });
+        torscraper.on('close', function () {
+          if (!id && output.indexOf('nothing new to download') !== -1) {
+            console.log('timer found nothing');
+          } else {
+            callback({
+              id: id,
+              output: output,
+              origin: origin
+            })();
+          }
+        });
+      }
+    }
+  }
+}
+
+exports.connect = function (callback) {
+  return function (token) {
+    return function () {
+      callback(new TelegramBot(token, {polling: true}))();
+      console.log('connected to Telegram');
+    }
+  }
+}
+
+exports.sendMessage = function(bot) {
+  return function (result) {
+    return function () {
+      var id = result.id;
+      var output = result.output;
+      var origin = result.origin;
+
+      if (output.length > 0) {
+        if (origin === 'timer' && output.indexOf('nothing new to download') !== -1) {
+          console.log('timer found nothing');
+        } else {
+          console.log(output);
+          bot.sendMessage(id, output);
+        }
+      }
+    }
+  }
+}
+
+exports.addMessagesListener = function (bot) {
+  return function (callback) {
+    bot.onText(/^get$/i, function (msg, match) {
+      var fromId = msg.from.id;
+      callback({
+        origin: 'request',
+        id: fromId
+      })();
+      console.log('got request from', fromId);
+    });
+  }
+}
+
+exports.interval = function (time) {
+  return function (id) {
+    return function (callback) {
+      var tick = function () {
+        callback({
+          origin: 'timer',
+          id: id
+        })();
+      };
+      tick();
+      setInterval(tick, time);
+    }
+  }
+}
---
+++ src/Main.purs
@@ -0,0 +1,84 @@
+module Main where
+
+import Prelude
+import Control.Monad.Aff (liftEff', makeAff, Aff, launchAff)
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Console (CONSOLE)
+import Control.Monad.Eff.Exception (EXCEPTION)
+import Node.Encoding (Encoding(UTF8))
+import Node.FS (FS)
+import Node.FS.Aff (readTextFile)
+import Node.Path (FilePath)
+
+type Origin = String
+type Token = String
+type Id = Int
+type Config =
+  { token :: Token
+  , torscraperPath :: FilePath
+  , master :: Id
+  }
+foreign import parseConfig :: String -> Config
+getConfig :: forall e. Aff (fs :: FS | e) Config
+getConfig = parseConfig <$> readTextFile UTF8 "./config.json"
+
+foreign import data TELEGRAM :: !
+type TelegramEffects e = (telegram :: TELEGRAM, console :: CONSOLE | e)
+foreign import data Bot :: *
+foreign import connect :: forall e.
+  (Bot -> Eff (TelegramEffects e) Unit) ->
+  Token ->
+  Eff (TelegramEffects e) Unit
+connect' :: forall e. Token -> Aff (TelegramEffects e) Bot
+connect' token = makeAff (\e s -> connect s token)
+
+foreign import sendMessage :: forall e. Bot -> Result -> Eff (TelegramEffects e) Unit
+
+type Request =
+  { origin :: String
+  , id :: Id
+  }
+
+foreign import addMessagesListener :: forall e.
+  Bot ->
+  (Request -> Eff (TelegramEffects e) Unit) ->
+  Eff (TelegramEffects e) Unit
+
+foreign import data TIMER :: !
+foreign import interval :: forall e.
+  Int ->
+  Id ->
+  (Request -> Eff (timer :: TIMER | e) Unit) ->
+  Eff (timer :: TIMER | e) Unit
+
+type Result =
+  { id :: Id
+  , output :: String
+  , origin :: Origin
+  }
+foreign import runTorscraper :: forall e.
+  FilePath ->
+  (Result -> Eff (console :: CONSOLE | e) Unit) ->
+  Request ->
+  Eff (console :: CONSOLE | e) Unit
+
+main :: forall e.
+  Eff
+    ( err :: EXCEPTION
+    , fs :: FS
+    , telegram :: TELEGRAM
+    , console :: CONSOLE
+    , timer :: TIMER
+    | e) Unit
+main = launchAff $ do
+  {token, torscraperPath, master} <- getConfig
+  bot <- connect' token
+
+  let addMessagesListener' = addMessagesListener bot
+  let interval' = interval (30 * 60 * 1000) master
+  let runTorscraper' = runTorscraper torscraperPath
+  let sendMessage' = sendMessage bot
+  let handleRequests = runTorscraper' sendMessage'
+
+  liftEff' $ addMessagesListener' $ handleRequests
+  liftEff' $ interval' $ handleRequests
